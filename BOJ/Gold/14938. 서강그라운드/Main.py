bf1ca0fa75d4dcfc040122f8dd5522b5235d85f7
prgmd <neon9008@gmail.com>
2026-02-04 09:41:59 +0900

    [Gold IV] Title: 서강그라운드, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/14938. 서강그라운드/Main.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+from collections import deque
+
+n, m, r = map(int, input().split())
+
+items = [0] + list(map(int, input().split()))
+
+graph = defaultdict(list)
+for _ in range(r):
+    a, b, l = map(int, input().split())
+    graph[a].append((b, l))
+    graph[b].append((a, l))
+
+answer = 0
+
+for item in range(1, len(items)):
+
+    q = deque()
+    q.append((item, m)) # 현재 위치와 남은 반경 입력
+
+    visited = set()
+    visited.add(item)
+
+    item_total = items[item]
+
+    while q:
+        now, left_length = q.pop()
+        if left_length <= 0:
+            continue
+
+        for nxt, length in graph[now]:
+            if length <= left_length and nxt not in visited:
+                item_total += items[nxt]
+                q.append((nxt, left_length-length))
+                visited.add(nxt)
+    
+    # answer와 값 비교
+    answer = max(item_total, answer)
+print(answer)
