ec6e6ab3c85c105898059908cac46250890afd3f
prgmd <neon9008@gmail.com>
2026-02-04 12:42:29 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2956 ms, Memory: 153644 KB - DashHub

--- BOJ/Gold/2206. 벽 부수고 이동하기/Main.py
+++ BOJ/Gold/2206. 벽 부수고 이동하기/Main.py
@@ -7,7 +7,6 @@ n, m = map(int, input().split())
 graph = [list(map(int, list(input().strip()))) for _ in range(n)]
 answer = int(1e9)
 d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-original_track = [[int(1e9) for _ in range(m)] for _ in range(n)]
 
 # 블록 좌표 체크
 blocks = []
@@ -16,43 +15,41 @@ for y in range(n):
         if graph[y][x] == 1:
             blocks.append((y, x))
 
+visited = [[[-1 for _ in range(m)] for _ in range(n)] for _ in range(2)]
+visited[0][0][0] = 1
+
 # 너비 탐색으로 최단 거리 측정
-def bfs(check):
+def bfs():
     global answer
 
     q = deque()
-    q.append((0, 0, 1))
-
-    visited = set()
-    visited.add((0, 0))
+    q.append((0, 0, 0))
 
     while q:
-        y, x, dist = q.popleft()
+        # case : 0 - 벽 안 부숨 / case : 1 - 벽 부숨
+        case, y, x = q.popleft()
 
         # n, m 도달시 최단 거리 비교하고 중단
         if (y, x) == (n-1, m-1):
-            answer = min(answer, dist)
             break
 
         for dy, dx in d:
             ny, nx = y + dy, x + dx
-            if 0 <= ny < n and 0 <= nx < m and (ny, nx) not in visited and graph[ny][nx] == 0 and dist <= original_track[ny][nx]:
-                q.append((ny, nx, dist+1))
-                visited.add((ny, nx))
-                if not check:
-                    original_track[ny][nx] = dist
+
+            if 0 <= ny < n and 0 <= nx < m:
+                if graph[ny][nx] == 0 and visited[case][ny][nx] == -1:
+                    visited[case][ny][nx] = visited[case][y][x] + 1
+                    q.append((case, ny, nx))
+                
+                elif graph[ny][nx] == 1 and case == 0:
+                    if visited[1][ny][nx] == -1:
+                        visited[1][ny][nx] = visited[0][y][x] + 1
+                        q.append((1, ny, nx))
+
 
 # 블록 제거 없이 한 번 실행
-bfs(False)
-if answer == int(1e9):
-    original_track = [[1e9 for _ in range(m)] for _ in range(n)]
-
-# 제외할 블록 선정
-for block in blocks:
-    y, x = block
-    graph[y][x] = 0
-    bfs(True)
-    graph[y][x] = 1
+bfs()
 
 # 답 출력
-print(-1 if answer == int(1e9) else answer)
+answer = max(visited[0][n-1][m-1], visited[1][n-1][m-1])
+print(answer)
