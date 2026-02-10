44d65c21a52cac2ad9abae495bedfdcbd76442ee
prgmd <neon9008@gmail.com>
2026-02-10 12:00:16 +0900

    [Bronze I] Title: 약수, Time: 44 ms, Memory: 32412 KB - DashHub

--- BOJ/Bronze/1037. 약수/Main.py
+++ BOJ/Bronze/1037. 약수/Main.py
@@ -1,16 +1,3 @@
 n = int(input())
 yaksu = list(map(int, input().split()))
-cnt = 2
-ans = int(1e9)
-while cnt < 1000001:
-    is_odd = True
-    for y in yaksu:
-        if y != cnt and cnt % y == 0:
-            continue
-        else:
-            is_odd = False
-            break
-    if is_odd:
-        print(cnt)
-        break
-    cnt += 1
+print(2 if n == 1 and yaksu[0] == 1 else max(yaksu)*min(yaksu))
