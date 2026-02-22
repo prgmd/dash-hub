402016b0b1074a19b5a18c8b483fc73ade1b12f9
prgmd <neon9008@gmail.com>
2026-02-22 18:26:34 +0900

    [Gold IV] Title: 좋은수열, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/2661. 좋은수열/Main.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+def check_repeat(stack):
+    i = 1
+    while 2*i <= len(stack):
+        if stack[-i:] == stack[-2*i:-i]:
+            return True
+        else:
+            i *= 2
+    return False
+
+def bt(n, stack):
+    if check_repeat(stack):
+        return
+    
+    if n == 0:
+        print(''.join(map(str, stack)))
+        exit()
+    
+    for x in [1, 2, 3]:
+        stack.append(x)
+        bt(n-1, stack)
+        stack.pop()
+
+    return
+
+bt(n, [])
