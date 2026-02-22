013315e9e9ad1d00091278ddcec04043fcb66a26
prgmd <neon9008@gmail.com>
2026-02-22 17:30:32 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 36 ms, Memory: 32412 KB - DashHub

--- BOJ/Bronze/2530. 인공지능 시계/Main.py
+++ BOJ/Bronze/2530. 인공지능 시계/Main.py
@@ -1,11 +1,11 @@
 h, m, s = map(int, input().split())
 s += int(input())
 
-while h > 23 or m > 60 or s > 60:
-    if s > 60:
+while h > 23 or m > 59 or s > 59:
+    if s >= 60:
         m += s // 60
         s %= 60
-    elif m > 60:
+    elif m >= 60:
         h += m // 60
         m %= 60
     elif h >= 24:
