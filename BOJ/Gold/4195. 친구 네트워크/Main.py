b5b24d16194e7fe3f187e6b4f7e7609185ac7317
prgmd <neon9008@gmail.com>
2026-02-26 14:27:59 +0900

    [Gold II] Title: 친구 네트워크, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/4195. 친구 네트워크/Main.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+
+def find(x):
+    if x not in d:
+        d[x] = x
+    if d[x] != x:
+        d[x] = find(d[x])
+    return d[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    d[a] = b
+
+def get_friend(a):
+    root = find(a)
+    cnt = 0
+    for key in d:
+        if find(d[key]) == root:
+            cnt += 1
+    return cnt
+
+for _ in range(t):
+    d = dict()
+    for _ in range(int(input())):
+        friend = list(input().split())
+        friend.sort()
+        a, b = friend[0], friend[1]
+        union(a, b)
+        print(get_friend(a))
