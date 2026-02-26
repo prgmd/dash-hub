7557a6988ff60b8549cd57a0ebbf392e49a7da50
prgmd <neon9008@gmail.com>
2026-02-26 12:25:47 +0900

    [Gold V] Title: 집합의 표현, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/1717. 집합의 표현/Main.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+parent = [i for i in range(n+1)]
+
+def find(x):
+    p = parent[x]
+    if p != x:
+        p = find(p)
+    return p
+
+def union(a, b):
+    n = find(a)
+    m = find(b)
+    if n > m:
+        parent[n] = m
+    else:
+        parent[m] = n
+
+for _ in range(m):
+    cmd, a, b = map(int, input().split())
+    
+    if cmd == 0:
+        union(a, b)
+    else:
+        if find(a) == find(b):
+            print('YES')
+        else:
+            print('NO')
