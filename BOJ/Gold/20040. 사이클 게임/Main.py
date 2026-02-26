4fc6ce72d5743c236ea2c17470a0b0e396b75618
prgmd <neon9008@gmail.com>
2026-02-26 16:17:27 +0900

    [Gold IV] Title: 사이클 게임, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/20040. 사이클 게임/Main.py
@@ -0,0 +1,32 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+parents = [i for i in range(n)]
+
+def find(x):
+    if parents[x] != x:
+        parents[x] = find(parents[x])
+    return parents[x]
+
+def union(a, b):
+    rootA = find(a)
+    rootB = find(b)
+    
+    if rootA == rootB:
+        return True
+    
+    if rootA < rootB:
+        parents[rootB] = rootA
+    else:
+        parents[rootA] = rootB
+    
+    return False
+
+ans = 0
+for i in range(m):
+    a, b = map(int, input().split())
+    if union(a, b):
+        ans = i+1
+
+print(ans)
