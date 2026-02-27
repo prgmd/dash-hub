e421cae750072cbf7ef2d812e7d7d8b46813deb1
prgmd <neon9008@gmail.com>
2026-02-27 11:11:43 +0900

    [Gold IV] Title: 스도쿠, Time: 3152 ms, Memory: 32412 KB - DashHub

--- BOJ/Gold/2239. 스도쿠/Main.py
+++ BOJ/Gold/2239. 스도쿠/Main.py
@@ -1,34 +1,38 @@
 sudoku = []
 blank = []
-for i in range(9):
-    sudoku.append(list(map(int, input())))
-    for j in range(9):
-        if sudoku[i][j] == 0:
-            blank.append((i, j))
 
-def can_sudoku(y, x, k):
-    for i in range(9):
-        if (i != x and sudoku[y][i] == k) or (i != y and sudoku[i][x] == k):
-            return False
-    for i in range(3):
-        for j in range(3):
-            if sudoku[(y//3)*3+i][(x//3)*3+j] == k:
-                return False
-    return True
+row_check = [[False] * 10 for _ in range(9)]
+col_check = [[False] * 10 for _ in range(9)]
+box_check = [[False] * 10 for _ in range(9)]
+
+for y in range(9):
+    sudoku.append(list(map(int, input())))
+    for x in range(9):
+        now = sudoku[y][x]
+        if now == 0:
+            blank.append((y, x))
+        else:
+            row_check[y][now] = True
+            col_check[x][now] = True
+            box_check[(y//3)*3 + (x//3)][now] = True
 
 def bt(n):
+    if n == len(blank):
+        for s in sudoku:
+            print(''.join(map(str, s)))
+        exit()
+
     for i in range(n, len(blank)):
         y, x = blank[i]
+        box_idx = (y//3)*3 + (x//3)
         for j in range(1, 10):
-            if can_sudoku(y, x, j):
+            if not row_check[y][j] and not col_check[x][j] and not box_check[box_idx][j]:
                 sudoku[y][x] = j
+                row_check[y][j] = col_check[x][j] = box_check[box_idx][j] = True
                 bt(n+1)
                 sudoku[y][x] = 0
+                row_check[y][j] = col_check[x][j] = box_check[box_idx][j] = False               
         else:
-            return
-
-    for s in sudoku:
-        print(''.join(map(str, s)))
-    exit()
-                    
+            return 
+                           
 bt(0)
