679e7e54fe12bf7fea47e2b924af6271a7c6730b
prgmd <neon9008@gmail.com>
2026-02-27 10:56:40 +0900

    [Gold IV] Title: 스도쿠, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Gold/2239. 스도쿠/Main.py
+++ BOJ/Gold/2239. 스도쿠/Main.py
@@ -1,28 +1,31 @@
-sudoku = [list(map(int, input())) for _ in range(9)]
+sudoku = []
+blank = []
+for i in range(9):
+    sudoku.append(list(map(int, input())))
+    for j in range(9):
+        if sudoku[i][j] == 0:
+            blank.append((i, j))
 
 def can_sudoku(y, x, k):
     for i in range(9):
-        if i != x and sudoku[y][i] == k:
+        if (i != x and sudoku[y][i] == k) or (i != y and sudoku[i][x] == k):
             return False
-        if i != y and sudoku[i][x] == k:
-            return False
-    for i in range((y//3)*3, (y//3)*3+3):
-        for j in range((x//3)*3, (x//3)*3+3):
-            if sudoku[i][j] == k:
+    for i in range(3):
+        for j in range(3):
+            if sudoku[(y//3)*3+i][(x//3)*3+j] == k:
                 return False
     return True
 
 def bt(n):
-    for i in range(n, 81):
-        y, x = i//9, i%9
-        if sudoku[y][x] == 0:
-            for j in range(1, 10):
-                if can_sudoku(y, x, j):
-                    sudoku[y][x] = j
-                    bt(n+1)
-                    sudoku[y][x] = 0
-            else:
-                return
+    for i in range(n, len(blank)):
+        y, x = blank[i]
+        for j in range(1, 10):
+            if can_sudoku(y, x, j):
+                sudoku[y][x] = j
+                bt(n+1)
+                sudoku[y][x] = 0
+        else:
+            return
 
     for s in sudoku:
         print(''.join(map(str, s)))
