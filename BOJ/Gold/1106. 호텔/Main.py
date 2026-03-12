1183753ffc7776aca2239d3642c4fe733c73df2b
prgmd <neon9008@gmail.com>
2026-03-12 15:28:17 +0900

    [Gold IV] Title: 호텔, Time: 36 ms, Memory: 32412 KB - DashHub

--- BOJ/Gold/1106. 호텔/Main.py
+++ BOJ/Gold/1106. 호텔/Main.py
@@ -7,21 +7,16 @@ def solve():
     for _ in range(n):
         cost, customer = map(int, input().split())
         hotel.append((cost, customer))
-    # hotel.sort(key=lambda x:(x[1]/x[0]), reverse=True)
 
     total = c+100
     dp = [int(1e9)] * (total)
     dp[0] = 0
 
-    for i in range(1, total):
-        for cost, customer in hotel:
-            if customer <= i:
-                dp[i] = min(dp[i], dp[i-customer]+cost)
+    for cost, customer in hotel:
+        for i in range(customer, total):
+            dp[i] = min(dp[i-customer]+cost, dp[i])
 
-    ans = int(1e9)
-    for i in range(c, total):
-        ans = min(ans, dp[i])
-
+    ans = min(dp[c:])
     print(ans)
 
 solve()
