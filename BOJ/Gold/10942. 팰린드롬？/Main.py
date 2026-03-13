2cb01f25b9d7f712b8d0f1ec23bedd5b687a7008
prgmd <neon9008@gmail.com>
2026-03-13 09:44:51 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1460 ms, Memory: 63700 KB - DashHub

--- BOJ/Gold/10942. 팰린드롬？/Main.py
+++ BOJ/Gold/10942. 팰린드롬？/Main.py
@@ -10,21 +10,13 @@ def solve():
     for i in range(n):
         dp[i][i] = 1
 
-    def is_pellin(x1, x2):
-        if board[x1:x2+1] == board[x1:x2+1][::-1]:
-            return True
-        return False
-
-    for y in range(n-1):
+    for y in range(n-1, -1, -1):
         for x in range(y+1, n):
-            if y != 0 and x != n-1:
+            if board[y] != board[x]:
                 continue
-            if is_pellin(y, x):
-                i, j = y, x
-                while i < j:
-                    dp[i][j] = 1
-                    i += 1
-                    j -= 1
+
+            if ((x-y) == 1 and dp[y+1][x] == 1) or (dp[y+1][x-1]):
+                dp[y][x] = 1
 
     for _ in range(int(input())):
         s, e = map(int, input().split())
