04ca30d78514320024f3c89efde713239c92da03
prgmd <neon9008@gmail.com>
2026-03-27 14:48:03 +0900

    [Platinum III] Title: 전설, Time: 5016 ms, Memory: 845404 KB - DashHub

--- BOJ/Platinum/19585. 전설/Main.py
+++ BOJ/Platinum/19585. 전설/Main.py
@@ -2,48 +2,38 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 color_trie = {}
-nick_trie = {}
+nicknames = set()
 
-def insert(word, trie, type):
+def insert(word, trie):
     node = trie
     for char in word:
         if char not in node:
             node[char] = {}
         node = node[char]
-    node[type] = True
+    node['#'] = True
 
 def search(word):
     node = color_trie
-    for idx, char in enumerate(word):
-        if char not in node:
-            if '.' in node and char in nick_trie:
-                node = nick_trie[char]
-            else:
-                return False
-        else:
-            if '.' in node:
-                if nick_search(word[idx-1:]):
-                    return True
-            node = node[char]
-    return '#' in node
-
-def nick_search(word):
-    node = nick_trie
-    for char in word:
+    for i in range(len(word)):
+        char = word[i]
+        if '#' in node:
+            if word[i:] in nicknames:
+                return True
         if char not in node:
             return False
         node = node[char]
-    return '#' in node
+    return False
 
 def solve():
     c, n = map(int, input().split())
     for _ in range(c):
-        insert(input(), color_trie, '.')
+        insert(input(), color_trie)
 
     for _ in range(n):
-        insert(input(), nick_trie, '#')
+        nicknames.add(input())
 
-    for _ in range(int(input())):    
-        print('Yes' if search(input()) else 'No')
+    teams = [input() for _ in range(int(input()))]
+    for team in teams:
+        print('Yes' if search(team) else 'No')
 
 solve()
