bcd9215402690a8da194fa8b17a8a4eb73864ee2
prgmd <neon9008@gmail.com>
2026-03-28 12:04:39 +0900

    [Gold III] Title: 파티, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Gold/1238. 파티/Main.py
+++ BOJ/Gold/1238. 파티/Main.py
@@ -3,47 +3,31 @@ import heapq
 n, m, x = map(int, input().split())
 input = sys.stdin.readline
 
-roads = {}
+roads = [[] for _ in range(n+1)]
 for _ in range(m):
     start, end, time = map(int, input().split())
-    if start not in roads:
-        roads[start] = []
     heapq.heappush(roads[start], (time, end))
 
-go_costs = [1e9 for _ in range(n+1)]
-back_costs = [1e9 for _ in range(n+1)]
+costs = [[1e9] * (n+1) for _ in range(n)]
+ans = [0] * (n+1)
 
 for i in range(1, n+1):
-    if i == x:
-        go_costs[i] = 0
-        continue
-
-    temp_costs = [1e9 for _ in range(n+1)]
-    temp_costs[i] = 0
+    row = costs[i-1]
+    row[i] = 0
 
     q = [i]
     while q:
         now = heapq.heappop(q)
         for dist, nxt in roads[now]:
-            if temp_costs[nxt] > temp_costs[now] + dist:
-                temp_costs[nxt] = temp_costs[now] + dist
+            if row[nxt] > row[now] + dist:
+                row[nxt] = row[now] + dist
                 if nxt == x:
                     continue
                 heapq.heappush(q, nxt)
-                
-    go_costs[i] = temp_costs[x]
 
-q = [x]
-back_costs[x] = 0
-
-while q:
-    now = heapq.heappop(q)
-    for dist, nxt in roads[now]:
-        if back_costs[nxt] > back_costs[now] + dist:
-            back_costs[nxt] = back_costs[now] + dist
-            heapq.heappush(q, nxt)
-
-ans = 0
 for i in range(1, n+1):
-    ans = max(ans, go_costs[i] + back_costs[i])
-print(ans)
+    if i == x:
+        continue
+    costs[x-1][i] += costs[i-1][x]
+
+print(max(costs[x-1][1:]))
